bd278b6f3a26d4209260e784780b638b1be18f5d
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-05-11 10:39:17 +0900

    2.1.1 , 2.1.2完成

---
+++ ConsoleApp1/Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //2.1.3
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
+            PrintSongs(songs);
+        }
+        //Mainメソッド内のPrintSongs(songs); をクリックしてAlt + Enterを押してメソッドを生成するを選択すると、以下のメソッドが自動的に作成される
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+
+
+
+
+
+
+        }
+    }
+}
