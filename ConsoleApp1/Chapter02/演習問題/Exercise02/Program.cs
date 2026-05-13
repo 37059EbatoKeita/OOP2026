62fecba845e6511488846c1f42b70cc06bc7d7d8
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-05-13 11:17:52 +0900

    2-2　未完成

--- ConsoleApp1/Chapter02/演習問題/Exercise02/Program.cs
+++ ConsoleApp1/Chapter02/演習問題/Exercise02/Program.cs
@@ -1,7 +1,12 @@
 ﻿namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            PrintinchToMeterList(1, 10);
+        }
+        // インチからメートルへの対応表を出力
+        private static void PrintinchToMeterList(int start, int end) {
+
+
         }
     }
 }
---
+++ ConsoleApp1/Chapter02/演習問題/Exercise02/incConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class incConverter {
+        private static readonly double ratio = 0.0254;
+
+        public static double Frominch(double inch) {
+            return inch / ratio;
+        }
+    }
+}
