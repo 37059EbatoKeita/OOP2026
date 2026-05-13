1f04e6fb51970c146c1534e6617c6e017f10e7b3
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-05-13 11:30:54 +0900

    2-2　模範解答

--- ConsoleApp1/Chapter02/演習問題/Exercise02/Program.cs
+++ ConsoleApp1/Chapter02/演習問題/Exercise02/Program.cs
@@ -4,9 +4,11 @@
             PrintinchToMeterList(1, 10);
         }
         // インチからメートルへの対応表を出力
-        private static void PrintinchToMeterList(int start, int end) {
-
-
+        private static void PrintinchToMeterList(int start, int stop) {
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = incConverter.ToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+            }
         }
     }
 }
--- ConsoleApp1/Chapter02/演習問題/Exercise02/incConverter.cs
+++ ConsoleApp1/Chapter02/演習問題/Exercise02/incConverter.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Exercise02 {
     internal class incConverter {
-        private static readonly double ratio = 0.0254;
+        private static readonly double ratio = 0.0254; //定数
 
-        public static double Frominch(double inch) {
-            return inch / ratio;
+        //メートルからインチを求める 
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+        //フィートからメートルを求める
+        public static double ToMeter(double inch) {
+            return inch * ratio;
         }
     }
 }
