278cd97d278e744af4699bf94e0bcfbe641c2fd1
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-05-13 10:44:17 +0900

    2.1.4 模範解答

--- ConsoleApp1/Chapter02/演習問題/Exercise01/Program.cs
+++ ConsoleApp1/Chapter02/演習問題/Exercise01/Program.cs
@@ -2,29 +2,43 @@
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
-
+            //歌データを入れるリストオブジェクトを生成
             var songs = new List<Song>();
 
+            //"**** 曲の登録 ****"を出力
+            Console.WriteLine(" **** 曲の登録 ****");
+
+            //何件入力があるかわからないので無限ループ
             while (true) {
-                //"アーティスト名："を出力
+                //曲名を出力
                 Console.Write("曲名:");
+                //入力された曲名を取得
                 String? title = Console.ReadLine();
 
-                if (title == "end") {
+                //endが入力されたら登録終了()
+                if (title.Equals("end", StringComparison.OrdinalIgnoreCase))
                     break;
 
-                    Console.Write("アーティスト名:");
-                    String? artistname = Console.ReadLine();
-                   
-                    Console.Write("演奏時間（秒）:");
-                    int length = Console.ReadLine());
+                //アーティスト名を出力
+                Console.Write("アーティスト名:");
+                //入力されたアーティスト名を取得
+                String? artistname = Console.ReadLine();
 
-                    Song song = new Song(title, artistname, length);
+                //演奏時間（秒）：を出力
+                Console.Write("演奏時間（秒）:");
+                //入力された演奏時間を取得
+                int length = int.Parse(Console.ReadLine());
 
-                    songs.Add(song);
+                //Songインスタンスを生成
+                Song song = new Song(title, artistname, length);
 
-                    PrintSongs(songs);
+                //歌データを入れるリストオブジェクトへ登録
+                songs.Add(song);
+
+                Console.WriteLine();   //改行
             }
+                PrintSongs(songs);
+        }
         //Mainメソッド内のPrintSongs(songs); をクリックしてAlt + Enterを押してメソッドを生成するを選択すると、以下のメソッドが自動的に作成される
         //2.1.4
         private static void PrintSongs(IEnumerable<Song> songs) {
