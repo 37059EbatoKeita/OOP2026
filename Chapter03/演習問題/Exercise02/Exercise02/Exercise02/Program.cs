58f4268aad683ead6aa92ca1f07021623ba7ca29
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-05-25 09:31:53 +0900

    問題3.2.4回答

--- Chapter03/演習問題/Exercise02/Exercise02/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Exercise02/Exercise02/Program.cs
@@ -49,8 +49,11 @@
 
         private static void Exercise2_4(List<string> cities) {
             //できたらGitのコメント「問題3.2.4完成」
-            var query = cities.Where(s => s.Length == 'B');
+            var query = cities.Where(s => s.StartsWith('B')).Select(s => s.Length);
+            foreach (var item in query) { 
+            Console.WriteLine(query);
         }
     }
+}
 }
 
