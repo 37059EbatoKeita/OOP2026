c0499c66aef301e59766034cb510af2a3ae4e53a
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-05-28 08:31:08 +0900

    4.2.1完成

---
+++ Chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" --- 4.2.1 ---");
+            Exercise1();
+            Console.WriteLine("\n --- 4.2.2 ---");
+            Exercise2();
+            Console.WriteLine("\n --- 4.2.3 ---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            //if-else文を使用
+            Console.Write("数値を入力してください" + ":");
+            string? inputNumber = Console.ReadLine();
+            if (!int.TryParse(inputNumber, out var number)) {
+                Console.WriteLine("入力値に誤りがあります");
+
+            }
+        }
+
+        private static void Exercise2() {
+            //switch文を使用
+
+        }
+
+        private static void Exercise3() {
+            //switch式を使用
+
+
+        }
+    }
+}
