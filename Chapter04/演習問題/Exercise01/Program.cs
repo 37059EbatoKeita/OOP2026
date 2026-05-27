6536abc066c613a480e8184c9dbfebf3007befe6
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-05-27 11:51:41 +0900

    4.1.3回答

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -50,10 +50,8 @@ namespace Exercise01 {
         }
 
         private static void Exercise3(List<string> langs) {
-            var lang = langs.Find(s => s.Length >= 10);
-
-
-            Console.WriteLine();
+            var lang = langs.Find(s => s.Length == 10) ?? "unknown";
+            Console.WriteLine(lang);
         }
     }
 }
