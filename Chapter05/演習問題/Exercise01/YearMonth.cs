ac814fb64da4235efb461308f8847d03c1cc639d
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-06-01 10:34:52 +0900

    5.1.1完成

---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+        public string Year { get; private set; }
+        public string Month { get; private set; }
+
+        public YearMonth(string Year, string Month) {
+            Year = Year;
+            Month = Month;
+        }
+        public void ChangeYear(string count) {
+            Year = count;
+        }
+        public void ChangeMonth(string count) {
+            Month = count;
+        }
+    }
+}
