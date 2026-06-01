770c34e433cdf1995a4abeb2654ca3ad894c3575
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-06-01 11:26:15 +0900

    5.1.2回答

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -7,15 +7,16 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     //5.1.1
     public class YearMonth {
-        public string Year { get; init; }
-        public string Month { get; init; }
+        public int Year { get; init; }
+        public int Month { get; init; }
 
         public YearMonth(string Year, string Month) {
             Year = Year;
             Month = Month;
         }
         //5.1.2
-        public bool Is21Century => YearMonth + Is21Century;
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
         }
     }
+
 
