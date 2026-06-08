0a2f54c8350d387717ed0d284de57dd3a4c9eb63
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-06-08 11:18:14 +0900

    引数で終了

---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exercise01;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var y1 = new YearMonth(2001, 12);
+            var y2 = new YearMonth(2001, 12);
+            if(y1 == y2) {
+                Console.WriteLine("y1とy2は等しい");
+            } else {
+                Console.WriteLine("y1とy2は等しくない");
+            }
+        }
+    }
+}
