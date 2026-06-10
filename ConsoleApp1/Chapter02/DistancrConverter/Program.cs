7cf6579400d99e537f305b4e7b70cf30b5cb31ab
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-06-10 10:52:22 +0900

    6.2　解答

--- ConsoleApp1/Chapter02/DistancrConverter/Program.cs
+++ ConsoleApp1/Chapter02/DistancrConverter/Program.cs
@@ -14,8 +14,6 @@ namespace DistancrConverter{
 
         //フィートからメートルへの対応表を出力
         static void PrintFeetToMeterList(int start, int stop){
-            
-
             for (int feet = start; feet <= stop; feet++){
                 double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");
@@ -23,8 +21,6 @@ namespace DistancrConverter{
         }
         //メートルからフィートへの対応表を出力
         static void PrintMeterToFeetList(int start, int stop){
-            
-
             for (int meter = start; meter <= stop; meter++){
                 double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine($"{meter}m = {feet:0.0000}ft");
