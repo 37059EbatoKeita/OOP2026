0703be5e0af23d88a46d4999c4f838a05090999c
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-06-11 10:49:04 +0900

    6.3.6　解答（ディクショナリ）

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -55,6 +55,31 @@ namespace Exercise03 {
         }
 
         private static void Exercise6(string text) {
+            var str = text.ToLower().Replace(" ","");
+
+            //辞書（ディクショナリ）を使う方法
+
+            //var alphDicCount = Enumerable.Range('a', 26).
+            //ToDictionary(num => ((char)num).ToString(), num => 0);
+
+            var dict = new SortedDictionary<char, int>();
+            foreach (var c in str) {
+                if (dict.ContainsKey(c))
+                    dict[c]++; //登録されている場合
+                else
+                    dict[c] = 1; //未登録の場合
+            }
+            foreach(var word in dict) {
+                Console.WriteLine(word.Key + ":" + word.Value);
+            }
+
+            //配列を用いた集計
+
+
+
+            //'a'から順にカウントして集計
+
+
 
         }
     }
