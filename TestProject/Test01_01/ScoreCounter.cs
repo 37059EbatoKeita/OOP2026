c2d99f447d392d18afb407966c97a668ea8ab0f2
江波戸 啓大 <oibc37059@ojs.ac.jp>
2026-06-22 11:33:52 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/ScoreCounter.cs
+++ TestProject/Test01_01/ScoreCounter.cs
@@ -6,31 +6,22 @@
         public ScoreCounter(string filePath) {
             _score = ReadScore(filePath);
         }
-
         //メソッドの概要：
         private static IEnumerable<Student> ReadScore(string filePath) {
             var sales = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            
 
-
-
-
-
-
-
+            foreach (var sale in sales) {
+            }
             return sales;
         }
 
         //メソッドの概要：
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
-
-
-
-
-
-
+            
+            foreach (var sale in dict){
+            }
             return dict;
         }
     }
